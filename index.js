20bf40362a47ef8fb3a50c2327b85d72200d4f6a
Njoki <kinuthiash@gmail.com>
2022-04-17 20:30:41 +0300

    commit tree 9e688dfd4e29c3d8f750d0d235a8ed1fa2e3651f

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+// remove main element with main id
+document.querySelector("main#main").remove();
+// create h1 element with victory id, inner text and class name
+const newHeader = document.createElement("hi");
+newHeader.id = "victory"
+newHeader.className = "victory"
+newHeader.innerHTML = "your name is the champion!"
+newHeader.nodeName = "h1"
